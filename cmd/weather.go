4cdbd69133608c989cd791aeec19ffc89f1057ea
Zach <Zacharydehooge@gmail.com>
2024-11-15 12:37:28 -0500

    Initial

---
+++ cmd/weather.go
@@ -0,0 +1,13 @@
+package weather
+
+import "fmt"
+
+func PullWeather() {
+	fmt.Println("Pulling weather data...")
+
+	// TODO: Implement weather data fetch
+	// TODO: Parse weather data
+	// TODO: Implement SLOGs
+
+	fmt.Println("Weather data pulled...")
+}
