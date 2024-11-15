9d8cab333ded38aacdc125ff352deca2fabb127b
Zach <Zacharydehooge@gmail.com>
2024-11-15 16:49:15 -0500

    modified:   cmd/weather.go

--- cmd/weather.go
+++ cmd/weather.go
@@ -1,13 +1,38 @@
 package weather
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// TODO: Implement weather data fetch (PullWeather Func)
+// TODO: Parse weather data
+// TODO: Implement SLOGs
 
 func PullWeather() {
+
 	fmt.Println("Pulling weather data...")
 
-	// TODO: Implement weather data fetch
-	// TODO: Parse weather data
-	// TODO: Implement SLOGs
+	response, err := http.Get("https://api.weather.gov/openapi.json")
+	if err != nil {
+		fmt.Println("Error fetching weather data:", err)
+		return
+	}
+	defer response.Body.Close()
 
-	fmt.Println("Weather data pulled...")
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	var data WeatherData
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+
 }
